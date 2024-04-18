39a1424278bcdde633529d6892155eb170e4d7ca
TheGrammerpro <theman1moh@gmail.com>
2024-04-18 19:06:21 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,119 @@
+from tkinter import *
+import pygame
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 0.25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer_countdown = None
+pygame.mixer.init()  # initialise the pygame module
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+
+def reset_timer():
+    global reps
+    reps = 0
+    window.after_cancel(timer_countdown)
+    timer.config(text="Timer")
+    canvas.itemconfig(timer_text, text="00:00")
+    start_button["state"] = "normal"
+    reset_button["state"] = "disabled"
+    checkmark_list.clear()
+    checkmark.config(text="")
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer.config(text='Long break', fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer.config(text='Short break', fg=PINK)
+    else:
+        count_down(work_sec)
+        timer.config(text='Work time', fg=GREEN)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def count_down(count):
+    global timer_countdown
+    start_button["state"] = "disabled"
+    reset_button["state"] = "normal"
+    count_minutes = math.floor(count / 60)
+    count_seconds = count % 60
+    if count_seconds < 10:
+        count_seconds = f"0{count_seconds}"
+
+    canvas.itemconfig(timer_text, text=f"{count_minutes}:{count_seconds}")
+    if count > 0:
+        timer_countdown = window.after(1000, count_down, count-1)
+    else:
+        notification_sound()
+        start_timer()
+        if reps % 2 == 0:
+            add_checkmark()
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# Canvas set up containing a tomato image with a timer:
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_face = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_face)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
+canvas.grid(column=1, row=1)
+
+# Buttons start and reset:
+start_button = Button(highlightthickness=0, command=start_timer)
+start_button.config(text="Start", font=(FONT_NAME, 12), fg="black", bg="white")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(highlightthickness=0, command=reset_timer)
+reset_button.config(text="reset", font=(FONT_NAME, 12), fg="black", bg="white")
+reset_button.grid(column=2, row=2)
+
+# Checkmark label:
+checkmark = Label(text="", fg=GREEN, bg=YELLOW, font=10)
+checkmark_list = []
+
+
+def add_checkmark():
+    checkmark_list.append("✔")
+    checkmarks = "".join(checkmark_list)
+    checkmark.config(text=checkmarks)
+    if len(checkmark_list) > 4:
+        checkmark_list.clear()
+
+
+checkmark.grid(column=1, row=3)
+
+# Timer label:
+timer = Label()
+timer.config(text="TIMER", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 26, 'bold'))
+timer.grid(column=1, row=0)
+
+
+def notification_sound():
+    pygame.mixer.music.load("notification-6175.mp3")
+    pygame.mixer.music.play(loops=0)
+
+
+window.mainloop()
